36a9dab1824e3b126eee7c1a3dfd81790a079601
Yunxin119 <1999yunxin@gmail.com>
2025-02-14 13:27:32 -0800

    server setup

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './connectDB.js';
+export const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// MARK: CORS
+const corsOptions = {
+    origin: true,
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// MARK: middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
