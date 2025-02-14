5421d54ba1274d311efa5bc757b8761cd7f7b07e
Yunxin119 <1999yunxin@gmail.com>
2025-02-13 16:32:03 -0800

    Project setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div className='w-screen h-screen overflow-y-scroll overflow-x-hidden'>
+        <h1 className='text-4xl text-center mt-10'>Welcome to MERN Stack</h1>
+    </div>
+  );
+}
+
+export default App;
