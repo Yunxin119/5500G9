1c5ed69771e66149ece75e81a8e7525c5c5a6ace
Yunxin119 <1999yunxin@gmail.com>
2025-02-13 16:36:55 -0800

    bg-settings and theme toggle settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ThemeToggle from './components/ThemeToggle';
 function App() {
   return (
     <div className='w-screen h-screen overflow-y-scroll overflow-x-hidden'>
-        <h1 className='text-4xl text-center mt-10'>Welcome to MERN Stack</h1>
+        <ThemeToggle />
     </div>
   );
 }
