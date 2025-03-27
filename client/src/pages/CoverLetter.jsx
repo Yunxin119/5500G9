11afb17ca241b444f3be784c9db162755d1c4d03
DLL <151422518+l1n9d@users.noreply.github.com>
2025-03-27 16:12:56 -0700

    update pages

--- client/src/pages/CoverLetter.jsx
+++ client/src/pages/CoverLetter.jsx
@@ -9,13 +9,22 @@ import userApiSlice from '../redux/userApiSlice'
 const CoverLetter = ({ currentUser }) => {
     const [resume, setResume] = useState(currentUser.resume || '');
     const [jd, setJd] = useState('');
+    const [pdfText, setPdfText] = useState('');
 
   return (
     <div className='screen'>
         <Navbar />
         <motion.div className='mt-8 relative left-1/2 transform -translate-x-1/2 flex flex-col md:flex-row justify-between items-center mt-4 backdrop-blur-md bg-gray-200/40 dark:bg-black/40 w-[80%] rounded-xl mb-3'>
-            <InfoInput setResume={setResume} setJd={setJd} />
-            <CoverLetterGeneration resume={resume} jd={jd} />
+            <InfoInput 
+              setResume={setResume} 
+              setJd={setJd} 
+              setPdfText={setPdfText} 
+            />
+            <CoverLetterGeneration 
+              resume={resume} 
+              jd={jd} 
+              pdfText={pdfText} 
+            />
         </motion.div>
     </div>
   )
